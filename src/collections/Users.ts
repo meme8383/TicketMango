bbfc3cda6cc5cc80dbaa14a03b3d2a2cfa236f44
Eduard Tanase <eduard.tanase1@gmail.com>
2024-05-17 15:09:59 -0700

    User permissions

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,9 +1,23 @@
-import { CollectionConfig } from 'payload/types';
+import { CollectionConfig, Access } from 'payload/types';
+
+const adminsAndUser: Access = ({ req: { user } }) => {
+  if (user?.role === 'admin') {
+    return true;
+  }
+
+  return {
+    id: {
+      equals: user?.id,
+    },
+  };
+};
 
 export const Users: CollectionConfig = {
   slug: 'users',
   auth: true,
   access: {
+    read: adminsAndUser,
+    create: () => true,
     delete: ({ req }) => req.user?.role === 'admin',
     update: ({ req }) => req.user?.role === 'admin',
   },
